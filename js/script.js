601c55b08d7f286cb869e8fb3ceb8eac56c9ca79
shawn kebel <fxrender@outlook.com>
2021-01-02 05:22:38 -0600

    Add 'body skin' light/dark option to project

--- js/script.js
+++ js/script.js
@@ -9,19 +9,19 @@
           totalPortfolioItem = portfolioItems.length;
 
 
-          for (let i=0; i<totalFilterBtn; i++) {
+          for (let i = 0; i < totalFilterBtn; i++) {
               filterBtns[i].addEventListener("click", function() {
                   filterContainer.querySelector(".active").classList.remove("active");
                   this.classList.add("active");
 
-                  const filterValue=this.getAttribute("data-filter");
+                  const filterValue = this.getAttribute("data-filter");
                   // noinspection InfiniteLoopJS
-                  for(let k=0; k<totalPortfolioItem; k++) {
-                      if(filterValue === portfolioItems[k].getAttribute("data-category")) {
+                  for (let k = 0; k < totalPortfolioItem; k++) {
+                      if (filterValue === portfolioItems[k].getAttribute("data-category")) {
                           portfolioItems[k].classList.remove("hide");
                           portfolioItems[k].classList.add("show");
                       }
-                      else{
+                      else {
                           portfolioItems[k].classList.remove("show");
                           portfolioItems[k].classList.add("hide");
                       }
--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,9 +1,10 @@
 
+    //  Style Switcher
     const links = document.querySelectorAll(".alternate-style"),
           totalLinks = links.length;
 
     function setActiveStyle(color) {
-        for (let i=0; i<totalLinks; i++) {
+        for (let i = 0; i < totalLinks; i++) {
             if(color === links[i].getAttribute("title")) {
                 links[i].removeAttribute("disabled");
             }
@@ -13,6 +14,22 @@
         }
     }
 
+    //  Body Skin
+    const bodySkin = document.querySelectorAll(".body-skin"),
+          totalBodySkin = bodySkin.length;
+        for (let i = 0; i < totalBodySkin; i++) {
+            bodySkin[i].addEventListener("change", function() {
+                if (this.value === "dark") {
+                    document.body.className = "dark";
+                }
+                else {
+                    document.body.className="";
+                }
+            })
+        }
+
+
+    //  Style Switcher Toggle
     document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
         document.querySelector(".style-switcher").classList.toggle("open");
     })
