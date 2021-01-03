0ae3601be45e6bcbe0c277a7af2f10b8f1a20a04
shawn kebel <fxrender@outlook.com>
2021-01-02 19:32:50 -0600

    Add functionality to 'hide' and 'show' sidebar with menu button

--- js/script.js
+++ js/script.js
@@ -89,6 +89,60 @@
         }
     })
 
+    // Side Navbar
+    const nav = document.querySelector(".nav"),
+          navList = nav.querySelectorAll("li"),
+          totalNavList = navList.length,
+          allSection = document.querySelectorAll(".section"),
+          totalSection = allSection.length;
+
+    for (let i = 0; i < totalNavList; i++) {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function() {
+            // Remove back section Class
+            for (let i = 0; i < totalSection; i++) {
+                allSection[i].classList.remove("back-section");
+            }
+
+            for (let j = 0; j < totalNavList; j++) {
+                if (navList[j].querySelector("a").classList.contains("active")) {
+                    // Add back section Class
+                    allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+
+        this.classList.add("active");
+        showSection(this);
+
+        if (window.innerWidth < 1200) {
+            sideSectionTogglerBtn();
+        }
+        })
+    }
+
+    function showSection(element) {
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active");
+    }
+
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+          side = document.querySelector(".side");
+
+    navTogglerBtn.addEventListener("click", sideSectionTogglerBtn)
+
+    function sideSectionTogglerBtn() {
+        side.classList.toggle("open");
+        navTogglerBtn.classList.toggle("open");
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.toggle("open");
+        }
+    }
+
+
 
 
 
