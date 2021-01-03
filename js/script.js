74ab78de2e5d05186f380f3109508e373cec1cf6
shawn kebel <fxrender@outlook.com>
2021-01-03 01:03:34 -0600

    Hide 'toggler' button at >1199px, link 'hire me' button to contact page, final styling adjustments

--- js/script.js
+++ js/script.js
@@ -108,14 +108,13 @@
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function() {
             // Remove back section Class
-            for (let i = 0; i < totalSection; i++) {
-                allSection[i].classList.remove("back-section");
-            }
+            removeBackSectionClass();
 
             for (let j = 0; j < totalNavList; j++) {
                 if (navList[j].querySelector("a").classList.contains("active")) {
                     // Add back section Class
-                    allSection[j].classList.add("back-section");
+                    addBackSectionClass(j);
+
                 }
                 navList[j].querySelector("a").classList.remove("active");
             }
@@ -129,6 +128,15 @@
         })
     }
 
+    function removeBackSectionClass() {
+       for (let i = 0; i < totalSection; i++) {
+           allSection[i].classList.remove("back-section");
+       }
+    }
+
+    function addBackSectionClass(num) {
+            allSection[num].classList.add("back-section");
+    }
     function showSection(element) {
         for (let i = 0; i < totalSection; i++) {
             allSection[i].classList.remove("active");
@@ -137,12 +145,31 @@
         document.querySelector("#" + target).classList.add("active");
     }
 
-    
+    function updateNav(element) {
+        for (let i = 0; i < totalNavList; i++) {
+            navList[i].querySelector("a").classList.remove("active");
+            const target = element.getAttribute("href").split("#")[1];
+            if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+                navList[i].querySelector("a").classList.add("active");
+            }
+        }
+
+    }
+
+    document.querySelector(".hire-me").addEventListener("click", function() {
+        const sectionIndex = this.getAttribute("data-section-index");
+        showSection(this);
+        updateNav(this);
+        removeBackSectionClass();
+        addBackSectionClass(sectionIndex);
+    })
 
     const navTogglerBtn = document.querySelector(".nav-toggler"),
           side = document.querySelector(".side");
 
-    navTogglerBtn.addEventListener("click", sideSectionTogglerBtn)
+    navTogglerBtn.addEventListener("click", () => {
+        sideSectionTogglerBtn();
+    })
 
     function sideSectionTogglerBtn() {
         side.classList.toggle("open");
